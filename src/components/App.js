504926b0be3238665f56ae6398158753b73c5b26
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2023-01-29 16:07:25 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -3,32 +3,34 @@ import '../styles/App.css';
 const App = () => {
 
   //code here 
-  // const [email, setEmail] = useState('')
-  // const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const inputEmailRef = useRef(null)
   const inputPasswordRef = useRef(null)
-  const [userDetails, setUserDetails] = useState({ email: '', password: '' })
-  function handleSubmit() {
-    if (inputEmailRef.current.value == '') {
+  const [userDetails, setUserDetails] = useState({})
+  const handleSubmit = () => {
+    if (email == '') {
       inputEmailRef.current.focus()
     }
-    else if (inputPasswordRef.current.value == '') {
+    else if (password == '') {
       inputPasswordRef.current.focus()
     }
     else {
       setUserDetails({
-        email: inputEmailRef.current.value,
-        password: inputPasswordRef.current.value
+        email,
+        password
       })
+      setEmail('')
+      setPassword('')
     }
 
   }
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" defaultValue={userDetails.email} ref={inputEmailRef} /><br />
+      <input id="inputEmail" type="text" value={email} ref={inputEmailRef} onChange={(e) => setEmail(e.target.value)} /><br />
       Password
-      <input id="inputPassword" type="text" defaultValue={userDetails.password} ref={inputPasswordRef} /><br />
+      <input id="inputPassword" type="text" value={password} ref={inputPasswordRef} onChange={(e) => setPassword(e.target.value)} /><br />
       <button id="submitButton" onClick={handleSubmit}>Submit</button><br />
       <p id="emailText">Your Email : {userDetails.email}</p>
       <p id="passwordText">Your Password : {userDetails.password}</p>
